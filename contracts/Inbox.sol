3b41454331307b139a0d5ef2cc5ccc1d557f4264
Nishaad <nknavkal@gmail.com>
2025-04-18 17:57:16 -0400

    rename

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -188,7 +188,7 @@ contract Inbox is IInbox, Ownable, Semver {
      * @param _localProver Address of prover on the destination chain
      * @param _sourceChainProver Address of prover on the source chain
      */
-    function messageBridgeSendBatch(
+    function sendFulfilled(
         uint256 _sourceChainID,
         bytes32[] calldata _intentHashes,
         address _localProver,
